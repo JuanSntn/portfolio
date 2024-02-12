62a302c6ec82bfce0ae6f79f6e8e28cdcb28f64d
JuanSntn <120034073@upq.edu.mx>
2024-02-12 14:22:05 -0600

    card en sobre mi

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import { FaLinkedin, FaGithub, FaWhatsapp } from 'react-icons/fa';
+
+
+
+
+
+
 
 const About = () => {
   return (
-    <div id="About" className="min-h-screen flex flex-col justify-center p-4 bg-white">
+    <div id="About" className="min-h-screen flex flex-col  justify-center p-4 bg-white">
       <div className="md:w-1/2 md:mb-0 md:mr-4 md:pl-20 flex-shrink-0 text-center mt-16 md:mt-0">
         {/* Ajusté el margen superior a mt-16 */}
         <h1 className="text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-black">
@@ -26,8 +33,42 @@ const About = () => {
             Me considero un desarrollador front-end muy responsable y meticuloso. Siempre me esmero por entregar trabajos de alto valor a mis clientes, respetando plazos y presupuestos. Estoy comprometido con la calidad de código y la optimización de la experiencia de usuario.
           </p>
         </div>
+
         <div className="md:w-1/2 text-left flex items-center justify-center">
-          <img src="./img/img1.svg" alt="Mi Icono" className="w-full md:w-auto h-[500px]" />
+          <div className="border mt-4 w-3/4 md:w-3/4 rounded-lg">
+            <div className="flex flex-col lg:flex-row items-center lg:items-start gap-6 p-8">
+              <img
+                src="./img/perfil.jpg"
+
+                alt="juan"
+                lazyload="true"
+                className="rounded-full w-24 h-24 object-cover ring-4 ring-gray-300"
+              />
+
+              <div>
+
+                <h3 className="font-semibold text-xl md:text-2xl text-gray-700">
+                  Juan Carlos León
+                </h3>
+                <p className="text-gray-600">Desarrollador full stack</p>
+              </div>
+            </div>
+            <div className="flex flex-col gap-4 px-6">
+              <p className="text-gray-500 text-center lg:text-left">
+                Apasionado por el desarrollo web, me especializo como full-stack developer con un enfoque destacado en el backend. Mi fascinación radica en la creación de sistemas robustos y seguros que respaldan la funcionalidad de aplicaciones web complejas.              </p>
+              <div className="flex items-center justify-center lg:justify-start gap-2 py-4">
+                <a href="https://www.linkedin.com/in/juan-carlos-leon-heredia-b197b3247/" target="_blank" className="p-2 bg-gray-100 rounded-lg">
+                  <FaLinkedin className="text-blue-900" size={25} />
+                </a>
+                <a href="https://github.com/JuanSntn" target="_blank" className="p-2 bg-gray-100 rounded-lg">
+                  <FaGithub className="text-gray-600" size={25} />
+                </a>
+                <a href="https://wa.me/7227981757" target="_blank" className="p-2 bg-gray-100 rounded-lg">
+                  <FaWhatsapp className="text-green-500" size={25} />
+                </a>                
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
